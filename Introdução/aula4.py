6e21c9c38b7566daaf90529f4e0a26f84abaad9a
root <root@localhost.localdomain>
2015-10-22 15:45:37 -0400

    Indentação em Python é obrigatória

---
+++ Introdução/aula4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""
+ Em ciência da computação, indentação (recuo, neologismo derivado da palavra em inglês indentation) é um termo aplicado ao código fonte de um programa para ressaltar ou definir a estrutura do algoritmo.
+
+Na maioria das linguagens de programação, a indentação é empregada com o objetivo de ressaltar a estrutura do algoritmo, aumentando assim a legibilidade do código. Em algumas linguagens, entretanto, a indentação é obrigatória. Python, Occam e Haskell, por exemplo, utilizam a mudança de indentação para definir a hierarquia dentre blocos de código.
+"""
+def ola():
+    print 'ola isso e uma funcao'
+
+ola()
