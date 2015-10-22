707cf02175b599460803e72041aa5609faa25ac0
root <root@localhost.localdomain>
2015-10-22 15:30:13 -0400

    Aula 3 operadores em matemática

---
+++ Introdução/aula3.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python
+
+#Funcoes matematicas
+
+#adicao
+a = 7+6
+
+# subtracao
+b= 4-3
+
+#multiplicacao
+
+c= 6*3
+
+# divisao
+d= 50/2
+
+#exponenciacao
+
+e = 5**4
+
+print a,b,c,d,e
+
+
