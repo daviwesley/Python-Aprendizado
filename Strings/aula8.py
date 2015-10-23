8301b9d5e2f25a1d2dd07dd9c0c3a87de7b69570
root <root@localhost.localdomain>
2015-10-23 11:17:33 -0400

    Acesso pelo índice de strings

---
+++ Strings/aula8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python env
+
+"""
+A string "PYTHON" tem seis caracteres,
+numerados de 0 a 5, como mostrado abaixo:
+Cada caractere em uma string recebe um numero.
+Este numero e chamado o indice.
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+Entao, se vc quisesse o "Y", poderia digitar
+"PYTHON"[1] (sempre comece contando a partir do 0!)
+"""
+
+c = "gatos"[0]
+n = "Ryan"[3]
+
+fifth_letter ="MONTY"[4]
+
+print c,n,fifth_letter
