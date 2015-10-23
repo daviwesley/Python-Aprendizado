6b7e914c30549db100cd294979293c4d1e831ca9
daviwesley <davi.wesley@icloud.com>
2015-10-23 12:02:35 -0300

    Métodos de string

--- Strings/aula9.py
+++ Strings/aula9.py
@@ -2,4 +2,14 @@
 
 """
 Metodos de string
+Vamos nos concentrar em quatro metodos de string:
+
+1.len()
+2.lower()
+3.upper()
+4.str()
 """
+
+parrot = "Azul Marinho"
+len(parrot)
+print len(parrot)
