a5a924d9ba919dedae85798f60be9a07d983f806
daviwesley <davi.wesley@icloud.com>
2015-10-23 12:57:13 -0300

    lower()
    Você pode usar o método lower() para se livrar de todas as letras
    maiusculas em suas strings

---
+++ Strings/aula10.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python env
+
+"""
+Você pode usar o metodo lower() para se livrar de 
+todas as letras maiusculas
+em suas strings. Voce chama lower()
+"""
+parrot = "Azul Marinho"
+
+print parrot.lower()
