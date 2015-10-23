904c9014aadc3a202693e8faff57bd1db3236f76
daviwesley <davi.wesley@icloud.com>
2015-10-23 14:02:31 -0300

    comentário adicionado

--- Exibição Avançada/aula18.py
+++ Exibição Avançada/aula18.py
@@ -2,5 +2,8 @@
 
 string_1 = "Camelot"
 string_2 = "lugar"
-
+"""
+%s =  indica que a variavel eh uma String
+% = referencia a variavel
+""""
 print "Nao vamos para %s. E um %s bobo." % (string_1, string_2)
