04cc961ce2c909f9108d113813fce4e100fcef51
root <root@localhost.localdomain>
2015-10-23 11:16:48 -0400

    Caracteres de escape

---
+++ Strings/aula7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python env
+
+"""
+Caracteres de escape
+uma aspas simples pode fazer com que seu codigo nao
+rode e ate mesmo acentos em portugues pode fazer a mesma
+coisa, uma solucao para as aspas simples eh colocar uma barra
+invertida \ antes da mesma
+"""
+
+print 'Neste calor so um copo d\'agua bem gelado.'
