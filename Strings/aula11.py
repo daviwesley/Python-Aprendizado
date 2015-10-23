ec2a937d892f876c01987f9bfb5ae7f1461033d8
daviwesley <davi.wesley@icloud.com>
2015-10-23 13:19:11 -0300

    uso do método upper()

---
+++ Strings/aula11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python env
+
+"""
+upper()
+Agora sua string esta 100% em letras minusculas! 
+Existe um metodo similar para deixar uma string 
+totalmente em letras maiusculas.
+"""
+
+parrot = "azul marinho"
+
+print parrot.upper()
