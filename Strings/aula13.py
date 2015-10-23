36a36d1361802fe8d05df824a1fcf98cc085802f
daviwesley <davi.wesley@icloud.com>
2015-10-23 13:22:28 -0300

    Notação de Ponto

---
+++ Strings/aula13.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python env
+"""
+Notação de Ponto
+Vamos examinar melhor por que voce usa len(string) 
+e str(object), mas notacao de ponto (como "String".upper()) para o resto.
+"""
+ministry = "Ministro do Caminhar Estranho"
+
+print len(ministry)
+print ministry.upper()
