eef58bc400640f2c20ac20c18775cbe21e9a11d9
daviwesley <davi.wesley@icloud.com>
2015-11-11 15:10:08 -0300

    Imprimindo data e hora

--- Biblioteca datetime/aula22.py
+++ Biblioteca datetime/aula22.py
@@ -3,4 +3,4 @@
 from datetime import datetime
 now = datetime.now()
 
-print '%s/%s/%s:%s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
