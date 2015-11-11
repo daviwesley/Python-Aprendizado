6acd20ac607c821a118a7b0b7afdbcc9ca0ce9cc
daviwesley <davi.wesley@icloud.com>
2015-11-11 15:25:20 -0300

    Expressões Booleanas

---
+++ Condicionais e Controle de Fluxo/aula24.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Crie declaracoes comparativas conforme apropriado nas linhas abaixo!
+
+# Torne-me verdade!
+bool_one = 3 < 5  # Ja fizemos este para voce!
+
+# Torne-me falso!
+bool_two = 54>900
+
+# Torne-me verdade!
+bool_three = 3==3
+
+# Torne-me falso!
+bool_four = 4<1
+
+# Torne-me verdade!
+bool_five = 99>8
