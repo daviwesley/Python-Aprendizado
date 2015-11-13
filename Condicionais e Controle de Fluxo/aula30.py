fea11cf094ea4e29d44d5a2dbf0e7212dbcb378d
daviwesley <davi.wesley@icloud.com>
2015-11-12 23:10:23 -0300

    Operadores booleanos not, or e and.

---
+++ Condicionais e Controle de Fluxo/aula30.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+bool_one = False or not True and True
+
+bool_two = False and not True or True
+
+bool_three = True and not (False or False)
+
+bool_four = not not True or False and not True
+
+bool_five = False or not (True and True)
---
+++ Condicionais e Controle de Fluxo/aula31.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Use expressoes booleanas nas linhas abaixo conforme apropriado!
+
+# Torne-me falso!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"  # Fizemos esta para você!
+
+# Torne-me verdadeiro!
+bool_two = (True or False)
+
+# Torne-me falso!
+bool_three = (5>2) and 9==8
+
+# Torne-me verdadeiro!
+bool_four = (not False) and True
+
+# Torne-me verdadeiro!
+bool_five = 7<10 and 9<11
