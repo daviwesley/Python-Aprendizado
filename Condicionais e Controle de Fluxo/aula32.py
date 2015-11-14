e7923df21783919306934498b61757a0168f2449
daviwesley <davi.wesley@icloud.com>
2015-11-13 23:19:48 -0300

    Usando if

---
+++ Condicionais e Controle de Fluxo/aula32.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+if some_function():
+    # linha um do bloco
+    # linha dois do bloco
+    # etc.
+'''
+
+def using_control_once():
+    if 8!=2:
+        return "Successo #1"
+
+def using_control_again():
+    if 10/2>3:
+        return "Successo #2"
+
+print using_control_once()
+print using_control_again()
