da112d28111db0f7685be3a0174114966431da00
daviwesley <davi.wesley@icloud.com>
2015-11-17 22:20:21 -0300

    Algoritimo para o Pig Latin

---
+++ Condicionais e Controle de Fluxo/aula39.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Vamos revisar as regras da tradução:
+
+Você move a primeira letra da palavra para o fim, e então adiciona o sufixo 'ay'.
+Exemplo: python -> ythonpay
+'''
+
+pyg = "ay"
