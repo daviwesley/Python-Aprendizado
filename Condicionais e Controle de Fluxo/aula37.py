77574e1a36ef6790b91bdb5e809d47fd6e2d4f64
daviwesley <davi.wesley@icloud.com>
2015-11-17 22:19:09 -0300

    Verificação de string

---
+++ Condicionais e Controle de Fluxo/aula37.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+print 'Bem-vindo ao Tradutor de Pig Latin!'
+
+# Comece seu codigo aqui!
+original = raw_input("Insira uma palavra")
+
+if len(original)>0:
+    print (original)
+else:
+    print ("Vazio")
+
