f1b181993318f6e75ae158ea9b638e075b82e155
daviwesley <davi.wesley@icloud.com>
2016-01-10 00:23:04 -0300

    Como definir uma função
    Funções são definidas com três componentes:
    
    O header (cabeçalho), que inclui a palavra-chave "def", o nome da função
    e quaisquer parâmetros de que a função precise. Por exemplo:
    
    def hello_world(): // There are no parameters
    Um comentário opcional que explica o que a função faz.
    
    """Prints 'Hello World!' to the console."""
    O corpo, que descreve os procedimentos que a função executa. O corpo é
    feito em parágrafos, assim como as declarações condicionais.

---
+++ Funções/aula43.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Defina sua funcao spam, comecando na linha 5. Voce
+# pode deixar o codigo na linha 11 de lado por enquanto -- vamos
+# explica-lo logo!
+
+def spam():
+    '''Prints ovos na tela'''
+    print 'Ovos!'
+
+
+
+# Defina a funcao spam acima desta linha.
+spam()
