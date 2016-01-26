66525c0a673b1d291796fbbfe084d146f3cde885
daviwesley <davi.wesley@icloud.com>
2016-01-26 00:54:15 -0300

    Definindo funções

---
+++ Funções/aula44.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def power(base,exponent):  # Adicione seus parametros aqui!
+    result = base**exponent
+    print "%d elevado a %d e %d." % (base, exponent, result);
+
+power(37,4)  # Adicione seus argumentos aqui!
