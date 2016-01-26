39057f5cb6fa65472f8bbb923d6e5e7bb2325bcb
daviwesley <davi.wesley@icloud.com>
2016-01-26 01:48:15 -0300

    Mostrar na tela as funções de um módulo

---
+++ Funções/aula49.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+'''
+Mostrando todas funcoes de um modulo na tela
+'''
+import math            # Importa o modulo math
+everything = dir(math) # Converte tudo em uma lista de coisas de math
+print everything       # Exibe tudo!
