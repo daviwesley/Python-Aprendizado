ba8154ce32bb9aed3802aba7eb3301fd08a1a918
Davi Wesley <davi.wesley@icloud.com>
2016-03-25 21:13:46 -0300

    função verificaPrimos adicionada

---
+++ Funções/conferirprimos.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Um numero primo eh um numero natural maior do que 1
+que tem apenas dois divisores positivos distintos, 1 e ele mesmo
+'''
+def verificaPrimos(num):
+    # numeros primos sao maiores do que 1
+    if num > 1:
+       # cria um laço para verificar o numero informado
+       for i in range(2,num):
+           if (num % i) == 0:
+               print num,"nao eh um numero primo"
+               break
+       else:
+           print num,"eh um numero primo"
+    # se o numero informado e menor que
+    # ou igual a 1, nao e um numero primo
+    else:
+        print num,"nao eh numero primo"
+
+num = int(input("Entre com um numero: "))
+verificaPrimos(num)
