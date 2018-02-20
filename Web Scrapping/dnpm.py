0d3ccbda590187b27dfc7757ab6d282d4cc1bece
Davi Wesley <davi.wesley@icloud.com>
2018-02-20 15:49:52 -0300

    Update dnpm.py

--- Web Scrapping/dnpm.py
+++ Web Scrapping/dnpm.py
@@ -1,19 +1,23 @@
 from bs4 import BeautifulSoup
-import re
+import sys
+
+#url = r"main.html"
 
 
-url = r"main.html"
-page = open(url, encoding="utf8")
-soup = BeautifulSoup(page.read(), "html.parser")
+for n in sys.argv[1:]:
+    page = open(n, encoding="utf8")
+    print(n)
+    soup = BeautifulSoup(page.read(), "html.parser")
+    table = soup.find('table', id="ctl00_conteudo_gridProcessos")
 
-data = []
-table = soup.find('table', id="ctl00_conteudo_gridProcessos")
-
-linhas = table.find_all("tr")
-
-for dados in linhas:
-    if dados.get_text().split()[6] == "Disponibilidade":
-        texto = " ".join(dados.get_text().split())
-        with open("lista.txt", "a") as arquivo:
-            arquivo.write(texto + "\n")
+    linhas = table.find_all("tr")
+    for dados in linhas:
+        print("passando aqui")
+        try:
+            if dados.get_text().split()[6] == "Licenciamento":
+                texto = " ".join(dados.get_text().split())
+                with open("lista.txt", "a") as arquivo:
+                    arquivo.write(texto + "\n")
+        except Exception:
+            pass
         #print(dados.get_text())
