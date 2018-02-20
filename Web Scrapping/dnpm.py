08ef6e7f4d150963124c1b1b1ba2931aff6fbe77
Davi Wesley <davi.wesley@icloud.com>
2018-02-20 09:18:11 -0300

    Create dnpm.py

---
+++ Web Scrapping/dnpm.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import re
+
+
+url = r"main.html"
+page = open(url, encoding="utf8")
+soup = BeautifulSoup(page.read(), "html.parser")
+
+data = []
+table = soup.find('table', id="ctl00_conteudo_gridProcessos")
+
+linhas = table.find_all("tr")
+
+for dados in linhas:
+    if dados.get_text().split()[6] == "Licenciamento":
+        print(dados.get_text())
