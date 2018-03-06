9a3896f7bba9520b5d25eb6971c9bfdfaa2274e9
Davi Wesley <davi.wesley@icloud.com>
2018-03-06 14:46:41 -0300

    função de quebrar captcha adicionado

--- Web Scrapping/dnpm.py
+++ Web Scrapping/dnpm.py
@@ -1,23 +1,46 @@
 from bs4 import BeautifulSoup
-import sys
+from PIL import Image
+import pytesseract
+import cv2
+import os
+import requests
 
-#url = r"main.html"
+url_base = "https://sistemas.dnpm.gov.br/SCM/Extra/site/admin/pesquisarProcessos.aspx"
+erro = "o código digitado não confere com o código da imagem."
 
+def processarImagem():
+    """
+    Aplica um tom de cinza na imagem e depois borra o fundo para facilitar
+    na leitura das letras. Retorna uma string com o resultado da leitura do
+    texto
+    """
+    image = cv2.imread("CaptchaImage.jpg")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.medianBlur(gray, 3)
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+    text = pytesseract.image_to_string(Image.open(filename))
+    os.remove(filename)
+    return text
 
-for n in sys.argv[1:]:
-    page = open(n, encoding="utf8")
-    print(n)
-    soup = BeautifulSoup(page.read(), "html.parser")
-    table = soup.find('table', id="ctl00_conteudo_gridProcessos")
+url = r"asdfasd.html"
+page = open(url, encoding="utf8")
+soup = BeautifulSoup(page.read(), "html.parser")
 
-    linhas = table.find_all("tr")
-    for dados in linhas:
-        print("passando aqui")
-        try:
-            if dados.get_text().split()[6] == "Licenciamento":
-                texto = " ".join(dados.get_text().split())
-                with open("lista.txt", "a") as arquivo:
-                    arquivo.write(texto + "\n")
-        except Exception:
-            pass
+table = soup.find('table', id="ctl00_conteudo_gridProcessos")
+print(processarImagem())
+linhas = table.find_all("tr")
+#linhas[3].find_all("td")[2].get_text().split()[0]
+
+for i in range(1,len(linhas)):
+    #print("tamanho = {}".format(len(dados)))
+    #print(len(linhas[i].find_all("td")))
+    valor = linhas[i].find_all("td")[2]
+    valorcampo = " ".join(valor.get_text().split())
+    #print("valor campo = {}".format(valorcampo))
+    if valorcampo == "Licenciamento":
+        #print("if verdadeiro")
+        texto = " ".join(linhas[i].get_text().split())
+        with open("lista.txt", "a") as arquivo:
+            arquivo.write(texto + "\n")
         #print(dados.get_text())
